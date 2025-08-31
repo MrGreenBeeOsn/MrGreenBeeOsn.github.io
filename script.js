c06507259869b1537702ae8361d8d2474983f57b
🐝 Mr. ßee - 🅾ng siêng năng📗 <130443637+MrGreenBeeOsn@users.noreply.github.com>
2025-08-31 16:32:33 +0000

    ye

--- script.js
+++ script.js
@@ -71,3 +71,17 @@ const backToTopButton = document.querySelector('.back-to-top');
                 }
             });
         });
+
+/* =============================
+   Scroll Into View (hash trong liên kết) - by GRK
+============================= */
+
+        window.addEventListener('load', () => {
+            const hash = window.location.hash;
+            if (hash) {
+                const targetElement = document.querySelector(hash);
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                }
+            }
+        });
