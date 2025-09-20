fc19200b0d42f002447ce48b028ad32235ef0db3
🐝 Mr. ßee - 🅾ng siêng năng📗 <130443637+MrGreenBeeOsn@users.noreply.github.com>
2025-09-20 17:57:46 +0000

    ye

--- my-react-app/src/assets/jsx/Footer.jsx
+++ my-react-app/src/assets/jsx/Footer.jsx
@@ -4,8 +4,10 @@ export default function Footer() {
   return (
 
     <footer>
-      <Link to="/"><ins>๏</ins></Link>
+      
       <h4 className="font-normal">© 2025 -<span className="bee">🐝</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mr. ßee - 🅾ng siêng năng📓</h4>
+      <Link to="/"><ins>๏</ins></Link>
+      
     </footer>
 
   );
