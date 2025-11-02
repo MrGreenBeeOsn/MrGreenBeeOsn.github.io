66034cbb0b2c901ef58a493d95268ea9e1dc1ba3
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-11-02 21:23:23 +0700

    ye

--- mrgreenbeeosn/src/2_main/msbeeosn/_indexes/ContactIndex.tsx
+++ mrgreenbeeosn/src/2_main/msbeeosn/_indexes/ContactIndex.tsx
@@ -12,6 +12,7 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { faBookOpenReader } from '@fortawesome/free-solid-svg-icons'
@@ -93,6 +94,13 @@ export default function ContactIndex(): React.JSX.Element {
               <FontAwesomeIcon icon={faWhatsapp} />
               <Link to="https://wa.me/84906920292?text=Hi!%20I'm%20available%20to%20talk%20about%20the%20Accounting%20opportunity." className="email-link" target="_blank">
                 WA Business
+              </Link>
+            </div>
+
+            <div className="contact-item">
+              <FontAwesomeIcon icon={faTelegram} />
+              <Link to="https://t.me/MrBeeOsn" className="email-link" target="_blank">
+                Telegram
               </Link>
             </div>
 
